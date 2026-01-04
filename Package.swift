3f3df96005eaf4155907d177eca6cebbc7b45907
Maundytime <maundy.time@gmail.com>
2026-01-04 23:32:01 +0800

    Rename package and targets to WechatOpenSDK

--- Package.swift
+++ Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "WechatOpenSDK-NoPay",
+    name: "WechatOpenSDK",
     platforms: [
         .iOS(.v17),
     ],
     products: [
-        .library(name: "WechatOpenSDK-NoPay", targets: ["WechatOpenSDK-NoPay"]),
+        .library(name: "WechatOpenSDK", targets: ["WechatOpenSDK"]),
     ],
     targets: [
         .binaryTarget(
-            name: "WechatOpenSDK-NoPay",
+            name: "WechatOpenSDK",
             url: "https://dldir1.qq.com/WechatWebDev/opensdk/XCFramework/OpenSDK2.0.5_NoPay.zip",
             checksum: "28f0eb2aae2ca35df6e545811890735fb8798cd31af99454a7ab2c203df43864"
         ),
